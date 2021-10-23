f0253d811b07b15d99232924a4a920c6050f7fbf
Aleksandr <amindmobile@gmail.com>
2021-10-23 21:57:00 +0300

    Implementing argparse (part 1)

---
+++ engine.py
@@ -0,0 +1,63 @@
+import ipaddress
+from zipfile import ZipFile
+import configparser
+from io import BytesIO
+import requests
+import os.path
+
+config = configparser.ConfigParser()
+config.read('settings.ini')
+
+
+class Blocklister:
+    def __init__(self):
+        self.url = ''
+        self.export = ''
+        self.list_name = ''
+
+    def _get_url(self):
+        response = requests.get(self.url)
+        archive = BytesIO(response.content)
+        return archive
+
+    # Read file inside archive
+    def _get_zip_contents(self):
+        with ZipFile(self._get_url()) as zip_file:
+            archived_file_name = zip_file.namelist()[0]
+            with zip_file.open(archived_file_name) as unzipped_file:
+                return unzipped_file.readlines()
+
+    # Cleanse trash
+    def _decode_ip(self):
+        decoded_ip = [ip_address.decode().strip() for ip_address in self._get_zip_contents()]
+        filtered_ip = set()
+        for ip in decoded_ip:
+            try:
+                if ipaddress.ip_network(ip):
+                    filtered_ip.add(ip)
+            except ValueError:
+                continue
+        return list(filtered_ip)
+
+    # Create firewall rules for Mikrotik
+    def _get_file(self, list_name):
+        firewall_string = 'ip firewall address-list add list=DROP address='  # change list name
+        with open(self.export, 'w') as mikrotik:
+            for subnet in self._decode_ip():
+                mikrotik_firewall_string = firewall_string+subnet
+                mikrotik.write('%s\n' % mikrotik_firewall_string)
+        if os.path.exists(self.export):
+            print(f'\nFile: {self.export} was exported and ready to use.')
+
+    def choose(self, list_name):
+        if list_name == 'bogon':
+            self.list_name = list_name
+            self.url = config['bogon']['url']
+            self.export = config['bogon']['export_file_name']
+            self._get_file(list_name)
+
+        elif list_name == 'drop':
+            self.list_name = list_name
+            self.url = config['drop']['url']
+            self.export = config['drop']['export_file_name']
+            self._get_file(list_name)
--- main.py
+++ main.py
@@ -1,70 +1,24 @@
-import ipaddress
-from zipfile import ZipFile
-import configparser
-from io import BytesIO
-import requests
+import argparse
+from engine import Blocklister
 
-config = configparser.ConfigParser()
-config.read('settings.ini')
-
-
-class Blocklister:
-    def __init__(self):
-        self.url = ''
-        self.export = ''
-        self.list_name = ''
-
-    def _get_url(self):
-        response = requests.get(self.url)
-        archive = BytesIO(response.content)
-        return archive
-
-    # Read file inside archive
-    def _get_zip_contents(self):
-        with ZipFile(self._get_url()) as zip_file:
-            archived_file_name = zip_file.namelist()[0]
-            with zip_file.open(archived_file_name) as unzipped_file:
-                return unzipped_file.readlines()
-
-    # Cleanse trash
-    def _decode_ip(self):
-        decoded_ip = [ip_address.decode().strip() for ip_address in self._get_zip_contents()]
-        filtered_ip = set()
-        for ip in decoded_ip:
-            try:
-                if ipaddress.ip_network(ip):
-                    filtered_ip.add(ip)
-            except ValueError:
-                continue
-        return list(filtered_ip)
-
-    # Create firewall rules for Mikrotik
-    def _get_file(self, list_name):
-        firewall_string = 'ip firewall address-list add list=DROP address='
-        with open(self.export, 'w') as mikrotik:
-            for subnet in self._decode_ip():
-                mikrotik_firewall_string = firewall_string+subnet
-                mikrotik.write('%s\n' % mikrotik_firewall_string)
-
-    def choose(self, list_name):
-        if list_name == 'bogon':
-            self.list_name = list_name
-            self.url = config['bogon']['url']
-            self.export = config['bogon']['export_file_name']
-            self._get_file(list_name)
-
-        elif list_name == 'drop':
-            self.list_name = list_name
-            self.url = config['drop']['url']
-            self.export = config['drop']['export_file_name']
-            self._get_file(list_name)
-
-
+parser = argparse.ArgumentParser(description='Create file with firewall rules.')
+parser.add_argument('list', type=str.lower, help='Name of the blacklist')
+args = parser.parse_args()
 download = Blocklister()
-download.choose('drop')
+
+if args.list == 'drop':
+    download.choose('drop')
+elif args.list == 'moon':
+    print('\n No, no! "Moon was just an example. You must use real name of the list."')
+elif args.list == 'bogon':
+    print('\n The blacklist "bogon" is not implemented yet.')
+
+else:
+    print('\nFor use please enter: python main.py list_name')
+    print('\nFor example: python main.py moon')
 
 
-
+# loguru
 
 
 
